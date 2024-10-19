c2484b7d7d0ffaeb92d3fac43b049425df28c45c
Ravidhote17 <ravidhote101@gmail.com>
2024-10-19 13:45:19 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+function readMore() {
+    document.getElementById('more-text').style.display = 'inline';
+    document.getElementById('read-more').style.display = 'none';
+    document.getElementById('read-less').style.display = 'inline';
+  }
+  
+  function readLess() {
+    document.getElementById('more-text').style.display = 'none';
+    document.getElementById('read-more').style.display = 'inline';
+    document.getElementById('read-less').style.display = 'none';
+  }
+  
+  
